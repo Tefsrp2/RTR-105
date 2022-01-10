1b4d9f68832a3f955d0f5655ccc22f2b1a3da773
Tefsrp2 <90239954+Tefsrp2@users.noreply.github.com>
2022-01-10 17:19:20 +0200

    Create ld3_kods.c

---
+++ ld3_derivative/ld3_kods.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    float a, b, c, d, e, x;
+    
+    printf("Lūdzu ievadiet a vērtību:\n");
+    scanf("%f", &a);
+    printf("Lūdzu ievadiet b vērtību:\n");
+    scanf("%f", &b);
+    printf("Lūdzu ievadiet c (precizitātes) vērtību:\n");
+    scanf("%f", &c);
+
+ printf("        x  ||       atan(-x)    ||   atan\'(-x)      ||   atan\'(-x)      ||    atan\'\'(-x)    ||   atan\'\'(-x)  \n"
+           "           ||                   ||analytical formula|| finite difference||analytical formula||finite difference\n"
+           "    ===========================================================================================================\n");
+  
+   x = a;
+   while(x < b){
+       
+       d=1/(pow(-x + c,2) + 1);
+       e=1/(pow(-x,2) + 1);
+       printf("%10.3f ||\t%10.3f\t||%10.3f\t   || %10.3f\t||%10.3f\t    ||%10.3f\n",x,atan(-x), 1/(pow(-x,2) + 1), (atan(-x) - atan(-x-c))/c, (-2 * (-x))/( pow(-x,4) +2 * pow(-x,2) + 1 ), d-e/c);
+        x+=c;
+   }
+}
+       
