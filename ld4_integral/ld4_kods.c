81aa45a12ed0a526935d387c194a4fc3fb276d33
Tefsrp2 <90239954+Tefsrp2@users.noreply.github.com>
2022-01-15 21:38:18 +0200

    Update ld4_kods.c

--- ld4_integral/ld4_kods.c
+++ ld4_integral/ld4_kods.c
@@ -3,7 +3,7 @@
 
 
 void main(){
-float a,b,c,eps,h,integr1=0,integr2, integr3;
+float a,b,c,h,eps,integr1=0,integr2, integr3, integr4;
 int k,n=2;
 
 printf("Ludzu ievadiet a vertību: \n");
@@ -37,6 +37,19 @@ scanf("%f", &eps);
 		    integr3=((-h)*atan(-a+k*h)+atan(-a+(k+1)*h))/2;
 	
 }
+   integr4 = -(b-a)*(-atan((a+b)/n)/2);
+    while(fabs(integr4-integr1)>eps)
+    {
+        n *= 2;
+        h = (-b-(-a))/n;
+        integr1 = integr4;
+        integr4 = 0;
+        for(k=0;k<n;k++)
+        {
+            integr4 += (-h)/3*(atan(-a)+4*(atan(-a)+atan(-b)/n)+atan(-b));
+        }
+    }
 printf("Integrala vertiba pec taisnsturu metodes: %f\n",integr2);
 printf("Integrala vertiba pec trapecu metodes: %f\n", integr3);
+printf("Integrala vertiba pec Simpsona metodes: %f\n", integr4);
 }
